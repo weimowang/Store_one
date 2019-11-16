2a86c580f567dde5c01dc3d6f7fa6334ff1e6867
WEIMOWANG <WEIMOWANG@WEIMOWANGde-MacBook-Pro.local>
2019-11-16 23:24:19 +0800

    add css and javascript

---
+++ asserts/scripts/main.js
@@ -0,0 +1,39 @@
+window.onload = function () {
+
+
+    document.getElementById('menubtn').addEventListener('click', function (e) {
+        $(this).toggleClass('active');
+        $('#menuUl').toggleClass('active');
+    })
+
+    $('#menuUl li').click(function () {
+        $(this).parent().find('li').each(function () {
+            if ($(this).hasClass('current-active')) {
+                $(this).toggleClass('current-active');
+            }
+        })
+        $(this).toggleClass('current-active');
+    })
+
+    $('.pagebtn').click(function () {
+        var _btnindex = $(this).index();
+
+        $('#wei-boxs').find('div').each(function () {
+            if ($(this).hasClass('active')) {
+                $(this).toggleClass('active');
+            }
+        })
+
+        $('#wei-boxs').children().eq(_btnindex).toggleClass('active');
+
+        $(this).parent().find('div').each(function () {
+            if ($(this).hasClass('active')) {
+                $(this).toggleClass('active');
+            }
+        })
+
+        $(this).toggleClass('active');
+    })
+
+}
+
